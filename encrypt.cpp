dd837ff640d38fcf4a0223605f14cbd9f27ad00e
Mini-Ware <codauknow@gmail.com>
2024-10-10 23:51:04 +0800

    added steps to encrypt

--- encrypt.cpp
+++ encrypt.cpp
@@ -1,16 +1,27 @@
 #include "encrypt.hpp"
+#include <openssl/evp.h>
+#include <openssl/aes.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 namespace encryption {
 
     EncryptionAlgorithm::EncryptionAlgorithm(const std::string& key) {
-        
+        string key = key;
     }
 
     std::string EncryptionAlgorithm::encrypt(const std::string& filename) {
         cout << "Encryption is selected - "+filename+"\n";
-        return filename;
+        // extract file contents
+        // encrypt the contents
+        // save the file
+        // extract file name
+        // encrypt file name
+        // rename the file
     }
 
     std::string EncryptionAlgorithm::decrypt(const std::string& filename) {
